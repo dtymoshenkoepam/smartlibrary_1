52cd70406c69296332aa3f53d6b1432638232aef
dtymoshenko <dmytro_tymoshenko@epam.com>
2023-04-04 13:39:11 +0200

    create the frame

---
+++ src/main/java/smart/library/smartlibrary_1/Smartlibrary1Application.java
@@ -0,0 +1,13 @@
+package smart.library.smartlibrary_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Smartlibrary1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Smartlibrary1Application.class, args);
+	}
+
+}
---
+++ src/test/java/smart/library/smartlibrary_1/Smartlibrary1ApplicationTests.java
@@ -0,0 +1,13 @@
+package smart.library.smartlibrary_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Smartlibrary1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
